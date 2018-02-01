3d26dccb8bc255e888fcd668538dcca4628b9066
965750 <aventhelast@gmail.com>
2018-02-01 13:53:09 +0100

    just php ajax added

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    include 'dbh.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +9,24 @@
     <title>Ajax</title>
 </head>
 <body>
-    
+    <h2>just testin</h2>
+    <div id="comments">
+        <?php
+            $sql = "SELECT * FROM comments LIMIT 2";
+            $result = mysqli_query($conn, $sql);
+            if(mysqli_num_rows($result) > 0){
+                
+                while($row = mysqli_fetch_assoc($result)){
+                    echo "<p>";
+                    echo $row['author'];
+                    echo "<br>";
+                    echo $row['message'];
+                    echo "</p>";
+                }
+            }else{
+                echo "there are no comments!";
+            }
+        ?>
+    </div>
 </body>
 </html>
